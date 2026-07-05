4b812a05c62addf9536aa4ee8075b0484a2389d6
Leo <leleonardleo4@gmail.com>
2026-07-05 20:51:43 +0100

    Quick changes

--- app/page.tsx
+++ app/page.tsx
@@ -12,6 +12,7 @@ export default function Page() {
         </div>
         <div className="font-mono text-xs text-muted-foreground">
           (Press <kbd>d</kbd> to toggle dark mode)
+          {/* Remove this comment */}
         </div>
       </div>
     </div>
